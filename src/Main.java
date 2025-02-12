94fb74b46666aa4dfe0a106625b3fe75e584076a
Ahmet Furkan ÖREK <33ahmetorek33@gmail.com>
2025-02-12 11:31:29 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] numbers = {1, 2, 3, 4, 5};
+        double a=0;
+        for (int i = 0; i < numbers.length; i++) {
+            a+=(1 / numbers[i]);
+
+        }
+        System.out.println(a);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
